1e51f7c8ac4f66f65d43884e4992c379bb39cf14
Valter Apolinario <valter_123@hotmail.com>
2024-06-26 13:10:57 -0300

    refact: ajuste de build

--- target/generated-sources/annotations/com/br/mobiauto/management/utils/mapper/StoreMapperImpl.java
+++ target/generated-sources/annotations/com/br/mobiauto/management/utils/mapper/StoreMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-26T11:24:44-0300",
+    date = "2024-06-26T13:10:17-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.6 (Azul Systems, Inc.)"
 )
 @Component
