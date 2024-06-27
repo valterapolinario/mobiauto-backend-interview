278596122dc4bef83e4b6e7b6ab49c7c6524db89
Valter Apolinario <valter_123@hotmail.com>
2024-06-27 17:09:30 -0300

    feat: realizado ajustes de config

--- src/main/java/com/br/mobiauto/management/configuration/SwaggerConfig.java
+++ src/main/java/com/br/mobiauto/management/configuration/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.br.mobiauto.management.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +16,15 @@ public class SwaggerConfig {
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("titulo teste")
-                        .description("description")
+                        .title(" Sistema Gestão facil")
+                        .description("Sistema para gestao de revenda de veiculos")
                         .version("1.0")
+                        .contact(new Contact()
+                                .name("Valter Apolinario")
+                                .email("valter_123@hotmail.com")
+                                .url("https://www.linkedin.com/in/valter-apolinario-156196150/"))
                         .termsOfService("http://termos teste")
-                        .summary("summary")
+                        .summary("Resolucao de desafio para Desenvolvedor Back-end para empresa mobiauto")
                         .license(new License()
                                 .name("MIT")));
     }
