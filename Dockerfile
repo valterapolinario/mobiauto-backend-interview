e477196131087d395d1fea3d23da4a335e1898d1
RodolfoGomes <rodolfoaguiargomes@gmail.com>
2024-06-28 17:40:08 -0300

    run app with docker

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,6 @@
 FROM maven:3.8.5-openjdk-17 AS build
-WORKDIR /app
-COPY pom.xml .
-COPY src ./src
-RUN mvn clean package -DskipTests
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} application.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","/application.jar"]
 
-FROM openjdk:17-jdk-slim
-WORKDIR /app
-COPY --from=build /app/target/mobiauto.management-0.0.1-SNAPSHOT.jar /app/app.jar
-ENTRYPOINT ["java", "-jar", "/app/app.jar"]
--- src/main/java/com/br/mobiauto/management/Application.java
+++ src/main/java/com/br/mobiauto/management/Application.java
@@ -3,13 +3,9 @@ package com.br.mobiauto.management;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync
-@EnableKafka
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 public class Application {
